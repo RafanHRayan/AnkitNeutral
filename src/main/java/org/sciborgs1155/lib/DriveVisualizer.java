c359e8a16bed2b6ca22bcb6d68aedf3ff74e73b7
TheAk5250 <kumara1@bxscience.edu>
2025-11-20 04:20:28 -0500

    Hydrogen Lite®

---
+++ src/main/java/org/sciborgs1155/lib/DriveVisualizer.java
@@ -0,0 +1,35 @@
+package org.sciborgs1155.lib;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+
+public class DriveVisualizer {
+    private double leftVoltageVolts;
+    private double rightVoltageVolts;
+
+    private final DifferentialDrivetrainSim simulation = new DifferentialDrivetrainSim(
+            DCMotor.getNEO(2), 7.29, 7.5,
+            60.0, Units.inchesToMeters(3), 0.7112,
+            VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
+
+    public DriveVisualizer() {
+        leftVoltageVolts = 0;
+        rightVoltageVolts = 0;
+    }
+
+    public void setLeftInput(double inputVoltage) {
+        leftVoltageVolts = inputVoltage;
+    }
+
+    public void setRightInput(double inputVoltage) {
+        rightVoltageVolts = inputVoltage;
+    }
+
+    public void update(double dtSeconds) {
+        simulation.setInputs(leftVoltageVolts, rightVoltageVolts);
+        simulation.update(dtSeconds);
+    }
+
+}
